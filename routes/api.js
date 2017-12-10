1be7ba493ed7ab454da0dbe81c5dddf4f6276f81
Richard Zhunio <Richard-284@hotmail.com>
2017-12-10 16:04:56 -0500

    Added new workouts

--- routes/api.js
+++ routes/api.js
@@ -6,74 +6,88 @@ let mw = require( '../lib/middleware' );
 let api = express.Router();
 
 // GET: /api/users/me
-api.get( '/users/me', auth.checkAuthenticated, (req, res) => {
+api.get( '/users/me', auth.checkAuthenticated, handleUser );
 
-    // If user is attached, an error probably occurred and was already sent
-    if ( !req.user ) return;
+function handleUser(req, res) {
 
-    db.readUsers( res, (err, users) => {
-        if ( err ) return;
+    db.readUsers( handleReadUsers );
+
+    function handleReadUsers(err, users) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         const { email, firstName, lastName } = users[req.user];
-        db.isTrainer( res, req.user, isTrainer => {
+        db.isTrainer( req.user, (err, isTrainer) => {
+            if ( err ) return mw.sendErrorMessage( res, err.message );
+
             res.json( { email, firstName, lastName, isTrainer: isTrainer } );
         } );
-
-
-    } );
-} );
+    }
+}
 
 // POST: /api/users/me
-api.post( '/users/me', auth.checkAuthenticated, (req, res) => {
+api.post( '/users/me', auth.checkAuthenticated, handleUpdateUser );
 
-    // If user is attached, an error probably occurred and was already sent
-    if ( !req.user ) return;
-
+function handleUpdateUser(req, res) {
     const id = req.user;
     const user = req.body.user;
 
-    db.readUsers( res, (err, users) => {
-        if ( err ) return;
+    db.readUsers( handleReadUsers );
+
+    function handleReadUsers(err, users) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         users[id].firstName = user.firstName;
         users[id].lastName = user.lastName;
+        users[id].workouts = [];
 
-        db.saveUsers( res, users, err => {
-            if ( err ) return;
+        db.saveUsers( users, handleSaveUserAsTrainer );
+    }
 
-            if ( req.body.isTrainer ) {
-                db.addTrainer( res, id, err => {
-                    if ( err ) return;
-                    mw.sendMessage( res, 'User updated successfully' );
-                } );
-            }
-        } );
+    function handleSaveUserAsTrainer(err) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
-    } );
-} );
+        if ( req.body.isTrainer ) {
+            db.addTrainer( id, err => {
+                if ( err ) return mw.sendErrorMessage( res, err.message );
+                mw.sendMessage( res, 'User updated successfully' );
+            } );
+        }
+    }
+}
 
-api.get( '/trainers', (req, res) => {
-    db.readTrainers( res, (err, trainers) => {
-        if ( err ) return;
+api.get( '/trainers', handleSendTrainers );
+
+function handleSendTrainers(req, res) {
+    db.readTrainers( (err, trainers) => {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         let trainerResponse = [];
 
-        trainers.forEach( trainer => {
+        trainers.forEach( formatTrainersResponse );
+        res.json( trainerResponse );
+
+        function formatTrainersResponse(trainer) {
             const { firstName, lastName, id } = trainer;
             trainerResponse.push( { firstName, lastName, id, workouts: [] } );
-        } );
-        res.json( trainerResponse );
+        }
     } );
-} );
+}
 
-api.get( '/trainers/workouts/:id', auth.checkAuthenticated, (req, res) => {
-    db.readTrainers( res, (err, trainers) => {
-        if ( err ) return;
+api.get( '/trainers/workouts/:id', auth.checkAuthenticated, handleWorkoutsByTrainer );
+
+function handleWorkoutsByTrainer(req, res) {
+    db.readTrainers( (err, trainers) => {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         const trainerId = parseInt( req.params.id );
         const trainer = trainers.find( trainer => trainer.id === trainerId );
-        db.readWorkouts( res, (err, workouts) => {
-            if ( err ) return;
+
+        if ( !trainer ) return mw.sendErrorMessage( res, 'Not trainer' );
+
+        db.readWorkouts( handleWorkouts );
+
+        function handleWorkouts(err, workouts) {
+            if ( err ) return mw.sendErrorMessage( res, err.message );
 
             const result = [];
             const { firstName, lastName, id } = trainer;
@@ -82,38 +96,75 @@ api.get( '/trainers/workouts/:id', auth.checkAuthenticated, (req, res) => {
                 trainer.workouts.forEach( i => result.push( workouts[i] ) );
 
             res.json( { firstName, lastName, id, workouts: result } );
-        } );
+        }
     } );
-} );
-
+}
 
 // GET: /api/workouts
-api.get( '/workouts', (req, res) => {
-    db.readWorkouts( res, (err, workouts) => {
-        if ( err ) return;
+api.get( '/workouts', handleWorkouts );
+
+function handleWorkouts(req, res) {
+    db.readWorkouts( (err, workouts) => {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         res.json( workouts );
     } );
-} );
+}
 
 // POST: /api/workouts
-api.post( '/workouts', (req, res) => {
-    db.readWorkouts( res, (err, workouts) => {
-        if ( err ) return;
+api.post( '/workouts/new', auth.checkAuthenticated, handleAddWorkout );
 
-        workouts.push( req.body );
-        db.saveWorkouts( res, workouts, err => {
-            if ( err ) return;
-            mw.sendMessage( res, 'Workout added successfully' );
-        } );
+function handleAddWorkout(req, res) {
+    let workoutIndex;
 
-    } );
-} );
+    db.readWorkouts( handleReadWorkouts );
+
+    function handleReadWorkouts(err, workouts) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
+
+        // Add workout to workouts database
+        workoutIndex = workouts.push( req.body.workout ) - 1;
+        db.saveWorkouts( workouts, handleSaveWorkouts );
+    }
+
+    function handleSaveWorkouts(err) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
+
+        // Add workout to user.workouts
+        db.readUsers( handleReadUsers );
+    }
+
+    function handleReadUsers(err, users) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
+
+        const user = users[req.user];
+        console.log( user );
+        user.workouts.push( workoutIndex );
+
+        db.saveUsers( users, handleSaveUsers );
+    }
+
+    function handleSaveUsers(err) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
+        return mw.sendMessage( res, 'Workout added successfully' );
+    }
+}
 
 // GET: /api/workouts/:workoutName
-api.get( '/workouts/:workoutName', (req, res) => {
-    db.readWorkouts( res, (err, workouts) => {
-        if ( err ) return;
+api.get( '/workouts/:workoutName', handleGetWorkout );
+
+api.use( handleErrors );
+
+function handleErrors(err, req, res, next) {
+    if ( err ) {
+        return mw.sendErrorMessage( res, err.message );
+    }
+    next( err );
+}
+
+function handleGetWorkout(req, res) {
+    db.readWorkouts( (err, workouts) => {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         let workoutName = req.params.workoutName;
         let workout = workouts.filter( workout => workoutUrl( workout.name ) === workoutName );
@@ -121,13 +172,12 @@ api.get( '/workouts/:workoutName', (req, res) => {
         if ( workout.length === 0 )
             mw.sendErrorMessage( res, 'Could not find ' + workoutName );
         else
-            db.saveWorkouts( res, workouts, err => {
-                if ( err ) return;
+            db.saveWorkouts( workouts, err => {
+                if ( err ) return mw.sendErrorMessage( res, err.message );
                 res.json( workout );
             } );
     } );
-} );
-
+}
 
 function workoutUrl(workoutName) {
     return workoutName.toLowerCase().split( ' ' ).join( '-' );
--- routes/auth.js
+++ routes/auth.js
@@ -8,78 +8,89 @@ let auth = express.Router();
 const SECRET = '123';
 
 // POST: /auth/register
-auth.post( '/register', (req, res) => {
-    db.readUsers( res, (err, users) => {
+auth.post( '/register', handleRegister );
+
+function handleRegister(req, res) {
+    db.readUsers( handleReadUsers );
+
+    function handleReadUsers(err, users) {
         // Error is handled appropriately in readUsers()
-        if ( err ) return;
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         const user = req.body;
 
         const id = users.push( user ) - 1;
         user.id = id;
 
-        db.saveUsers( res, users, err => {
-            if ( err ) return;
-            let token = signToken( user );
-            sendToken( res, user, token );
-        } );
-    } );
-} );
+        db.saveUsers( users, handleSaveUser );
+    }
+
+    function handleSaveUser(err) {
+        if ( err ) return mw.sendErrorMessage( res, err.message );
+        let token = signToken( user );
+        sendToken( res, user, token );
+    }
+}
 
 /* POST: /auth/login
  * req.body: {object} user
  * req.body: {string} user.email
  * req.body: {string} user.password
  */
-auth.post( '/login', (req, res) => {
+auth.post( '/login', handleLogin );
 
-    db.readUsers( res, (err, users) => {
+function handleLogin(req, res) {
+    db.readUsers( handleReadUsers );
+
+    function handleReadUsers(err, users) {
         // Error is handled appropriately in readUsers()
-        if ( err ) return;
+        if ( err ) return mw.sendErrorMessage( res, err.message );
 
         // Make sure user property exists
-        if ( !req.body.user ) {
-            mw.sendErrorMessage( res, 'No login information provided' );
-            return;
-        }
+        if ( !req.body.user )
+            return mw.sendErrorMessage( res, 'No login information provided' );
 
         // Find user by email
-        const user = users.find( user => user.email === req.body.user.email.toLowerCase() );
+        const user = users.find( findUserByEmail );
 
         // Make sure user is found
-        if ( !user ) {
-            mw.sendErrorMessage( res, `The email ${req.body.user.email} does not exists` );
-            return;
-        }
+        if ( !user )
+            return mw.sendErrorMessage( res, `The email ${req.body.user.email} does not exists` );
 
         // Validate password
-        if ( user.password !== req.body.user.password ) {
-            mw.sendErrorMessage( res, 'Invalid email or password.' );
-            return;
-        }
+        if ( user.password !== req.body.user.password )
+            return mw.sendErrorMessage( res, 'Invalid email or password.' );
 
         // Sign token using JWT
         const token = signToken( user );
 
         // Send user after passing all checks
         sendToken( res, user, token );
-    } );
-} );
+    }
 
+    function findUserByEmail(user) {
+        return user.email === req.body.user.email.toLowerCase();
+    }
+}
 
 function checkAuthenticated(req, res, next) {
-    if ( !req.header( 'authorization' ) )
-        mw.sendErrorMessage( res, 'Missing authentication header' );
+    let err;
+
+    if ( !req.header( 'authorization' ) ) {
+        err = new Error( 'Missing authentication header' );
+        return next( err );
+    }
 
     const token = req.header( 'authorization' ).split( ' ' )[1];
 
     const payload = decodeToken( token );
 
-    if ( !payload )
-        mw.sendErrorMessage( res, 'Unauthorized request. Authentication header is invalid' );
-    else
-        req.user = payload.user;
+    if ( !payload ) {
+        err = new Error( 'Unauthorized request. Authentication header is invalid' );
+        return next( err );
+    }
 
+    req.user = payload.user;
     next();
 }
 
@@ -92,7 +103,7 @@ function decodeToken(token) {
 }
 
 function sendToken(res, user, token) {
-    res.json( { name: user.firstName, token: token } );
+    res.json( { name: user.firstName + ' ' + user.lastName, token: token } );
 }
 
 module.exports = auth;
